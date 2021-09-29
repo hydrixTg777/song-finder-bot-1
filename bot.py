99323ac75804d7001224f1cf577ab228a2013b72
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-09-30 00:20:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,28 +4,92 @@ import requests
 import subprocess
 from requests import get
 
-from MashaRoBot.services.pyrogram import pbot
-from pyrogram import filters
-from MashaRoBot.pyrogramee.pluginshelper import edit_or_reply, get_text
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-WOLFRAM_ID = os.environ.get("WOLFRAM_ID", None)
+RSR = Client(
+    "Answer Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    WOLFRAM_ID = os.environ["WOLFRAM_ID"]
+)
 
+START = """ Hi {}, I'm Join Sticker Sender Bot.\n\n
+You can get answer for your question using me🙂
+"""
 
-@pbot.on_message(filters.command(["zawhna", "ask"]))
-async def ask_friday(client, message):
-    zote = await edit_or_reply(message, "`Typing...`")
-    io = get_text(message)
-    if not io:
-        await zote.edit("Gimme Query🤨\n\n**Example👇**\n\n`/zawhna What is Atom?`")
-        return
+ABOUT = """
+● **OWNER :** [RSR](https://t.me/rsrmusic) 
+● **SERVER :** `Heroku` 
+● **LIBRARY :** `Pyrogram` 
+● **LANGUAGE :** `Python 3.9` 
+"""
+
+HELP = """
+Ask me something(i accpeted text only)
+"""
+
+START_BTN = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('About',callback_data='rsrabout'),
+        InlineKeyboardButton('Help',callback_data='rsrhelp')
+        ],
+        [
+        InlineKeyboardButton('Join Channel', url='https://t.me/mizolibrary'),
+        InlineKeyboardButton('Source', url='https://github.com/RSR-TG-Info/Join-Sticker'),
+        ],
+        [InlineKeyboardButton('Owner', url="t.me/rsrmusic")
+        ]]
+        
+    )
+BACK_BTN = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Back',callback_data='rsrback'),
+        ]]
+    )
+
+@RSR.on_callback_query()
+async def cb_data(bot, update):  
+    if update.data == "rsrhelp":
+        await update.message.edit_text(
+            text=HELP,
+            reply_markup=BACK_BTN,
+            disable_web_page_preview=True
+        )
+    elif update.data == "rsrabout":
+        await update.message.edit_text(
+            text=ABOUT,
+            reply_markup=BACK_BTN
+        )
+    else:
+        await update.message.edit_text(
+            text=START.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BTN
+        )
+        
+@RSR.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    text = START.format(update.from_user.mention)
+    reply_markup = START_BTN
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+@RSR.on_message(filters.text & filters.private)
+async def ask(bot, update):
     ques = urllib.parse.quote_plus(io)
     appid = WOLFRAM_ID
     server = f"https://api.wolframalpha.com/v1/spoken?appid={appid}&i={ques}"
     res = get(server)
     if "Wolfram Alpha did not understand" in res.text:
-        await zote.edit(
-            "**Sorry,i couldn't recognized your question**\n\nI zawhna chhan na hi ka hmuzo lo😔"
+        await bot.send_chat_action(message.chat.id, "Typing")
+        await bot.send_message(
+            "**Sorry,i couldn't find answer for your question**😔"
         )
         return
-    await zote.edit(res.text, parse_mode="markdown")
+    await bot.send_message(res.text, parse_mode="markdown")
