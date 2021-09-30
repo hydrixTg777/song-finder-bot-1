6ef80fbc5e6e56e1c71f1df03d37f21fa8fb3593
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-09-30 11:16:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,15 @@
 import os
-import urllib
+import asyncio
+from shazamio import Shazam
+from pyrogram import filters
+import datetime
 import requests
-import subprocess
-import pyrogram
-import io
-from requests import get
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import time
 
-from pyrogram.types import (
-
-  Message, 
-
-)
-
-from pyrogram import filters, Client 
-
-import requests
-
-import json
-
-
-WOLFRAM_ID = os.environ.get("WOLFRAM_ID", None)
 
 
 RSR = Client(
-    "Answer Bot",
+    "Audify Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
@@ -98,17 +82,72 @@ async def start(bot, update):
 
 
 
-@RSR.on_message(filters.text & filters.private)
-async def kukiai(client: Client, message: Message):
-  msg = message.text
-  chat_id = message.chat.id
+async def shazam(file):
+    shazam = Shazam()
+    try:
+        r = await shazam.recognize_song(file)
+    except:
+        return None, None, None
+    if not r:
+        return None, None, None
+    track = r.get("track")
+    nt = track.get("images")
+    image = nt.get("coverarthq")
+    by = track.get("subtitle")
+    title = track.get("title")
+    return image, by, title
 
-  Kuki =   requests.get(f"https://kuki-api.tk/api/botname/owner/message={msg}").json()
+async def convert_to_audio(vid_path):
+    stark_cmd = f"ffmpeg -i {vid_path} -map 0:a friday.mp3"
+    await runcmd(stark_cmd)
+    final_warner = "friday.mp3"
+    if not os.path.exists(final_warner):
+        return None
+    return final_warner
 
-  moezilla = f"{Kuki['reply']}"
-      
-  await client.send_chat_action(message.chat.id, "typing")
-  await message.reply_text(moezilla)
+@friday_on_cmd(
+    ["shazam"],
+    cmd_help={
+        "help": "Recognize / Discover A Song",
+        "example": "{ch}shazam (reply to music file)",
+    },
+)
+async def shazam_(client, message):
+    stime = time.time()
+    rsr1 = await message.reply("🔍")
+    if not message.reply_to_message:
+        return await rsr1.edit("`Reply Audio or Video`")
+    if not (message.reply_to_message.audio or message.reply_to_message.voice or message.reply_to_message.video):
+        return await rsr1.edit("`Reply to Audio File`")
+    if message.reply_to_message.video:
+        video_file = await message.reply_to_message.download()
+        music_file = await convert_to_audio(video_file)
+        dur = message.reply_to_message.video.duration
+        if not music_file:
+            return await rsr1.edit("`Unable to convert to Song File. Is this a valid File?`")
+    elif (message.reply_to_message.voice or message.reply_to_message.audio):
+        dur = message.reply_to_message.voice.duration if message.reply_to_message.voice else message.reply_to_message.audio.duration
+        music_file = await message.reply_to_message.download()
+    size_ = humanbytes(os.stat(music_file).st_size)
+    dur = datetime.timedelta(seconds=dur)
+    thumb, by, title = await shazam(music_file)
+    if title is None:
+        return await rsr1.edit("`No results found`")
+    etime = time.time()
+    t_k = round(etime - stime)
+    caption = f"""<b><u>Identified Finish ✅</b></u>
+    
+<b>📂Song Name :</b> <code>{title}</code>
+<b>🎙️Artist :</b> <code>{by}</code>
+<b>⏳Duration :</b> <code>{dur}</code>
+<b>🗂️Size :</b> <code>{size_}</code>
+<b>⏰Time Taken :</b> <code>{t_k} Seconds</code>
+    """
+    if thumb:
+        await rsr1.delete()
+        await message.reply_to_message.reply_photo(thumb, caption=caption, quote=True)
+    else:
+        await rsr1.edit(caption)
 
     
 RSR.run()
