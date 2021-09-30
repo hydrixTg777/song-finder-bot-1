1bfd7e95fc84443d159c9cf066e4b29637d72851
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-09-30 14:39:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,7 +92,7 @@ async def shazam(file):
         return None, None, None
     track = r.get("track")
     if not track:
-        RSR.send_message("**Song not found☹️**")
+        RSR.reply_text("**Song not found☹️**")
     nt = track.get("images")
     image = nt.get("coverarthq")
     by = track.get("subtitle")
