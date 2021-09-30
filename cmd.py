add49aba2fd482a5e0b5c7918a579cff886bf3a0
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-09-30 13:17:57 +0530

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -3,8 +3,9 @@ from typing import Tuple
 
 
 
-async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
-    """Run Commands"""
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
     args = shlex.split(cmd)
     process = await asyncio.create_subprocess_exec(
         *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
@@ -16,3 +17,30 @@ async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
         process.returncode,
         process.pid,
     )
+
+
+async def fetch_audio(client, message):
+    """Fetch Audio From Videos Or Audio Itself"""
+    c_time = time.time()
+    if not message.reply_to_message:
+        await message.edit("**Reply Audio or Video🙄**")
+        return
+    warner_stark = message.reply_to_message
+    if warner_stark.audio is None and warner_stark.video is None:
+        await message.edit("**Format not Supported🤕**")
+        return
+    if warner_stark.video:
+        await message.edit("**Video Detected, Converting to Audio😊**")
+        warner_bros = await message.reply_to_message.download(
+            progress=progress, progress_args=(message, c_time, f"**Downloading Audio😁**")
+        )
+        stark_cmd = f"ffmpeg -i {warner_bros} -map 0:a friday.mp3"
+        await runcmd(stark_cmd)
+        final_warner = "friday.mp3"
+    elif warner_stark.audio:
+        await message.edit("**Download Started🙃**")
+        final_warner = await message.reply_to_message.download(
+            progress=progress, progress_args=(message, c_time, f"**Downloading Video😁**`")
+        )
+    await message.edit("**Almost Done🤭**")
+    return final_warner
