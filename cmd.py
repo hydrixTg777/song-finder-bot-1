3215d35fd2b0e0f8812ee8b960fa77ec0f585cde
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-05-10 20:33:56 +0530

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -19,37 +19,3 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
         process.returncode,
         process.pid,
     )
-
-
-async def fetch_audio(client, message):
-    time.time()
-    if not message.reply_to_message:
-        await message.reply("**Reply Video or Audio**")
-        return
-    warner_stark = message.reply_to_message
-    if warner_stark.audio is None and warner_stark.video is None:
-        await message.reply("**Format not Supported**")
-        return
-    if warner_stark.video:
-        rsr2 = await message.reply("**Converting😁**")
-        warner_bros = await message.reply_to_message.download()
-        stark_cmd = f"ffmpeg -i {warner_bros} -map 0:a friday.mp3"
-        await runcmd(stark_cmd)
-        final_warner = "friday.mp3"
-    elif warner_stark.audio:
-        rsr2 = await edit_or_reply(message, "**Downloading🤭**")
-        final_warner = await message.reply_to_message.download()
-    await rsr2.edit("**Finished😊**")
-    await rsr2.delete()
-    return final_warner
-
-
-async def edit_or_reply(message, text, parse_mode="md"):
-    if message.from_user.id:
-        if message.reply_to_message:
-            kk = message.reply_to_message.message_id
-            return await message.reply_text(
-                text, reply_to_message_id=kk, parse_mode=parse_mode
-            )
-        return await message.reply_text(text, parse_mode=parse_mode)
-    return await message.edit(text, parse_mode=parse_mode)
